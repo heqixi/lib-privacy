2fa9b8ab2314033fe0f536ce775346604ccd6d75
zhenglingxiao <zhenglingxiao@cvte.com>
2020-09-20 22:26:35 +0800

    [feature] 添加进入沉浸模式的方法

--- utils/src/main/java/com/seewo/student/libutils/utils/ScreenUtil.kt
+++ utils/src/main/java/com/seewo/student/libutils/utils/ScreenUtil.kt
@@ -34,6 +34,32 @@ class ScreenUtil {
             return statusBarHeight
         }
 
+        fun enterImmersive(activity: Activity) {
+            activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            activity.window.statusBarColor = Color.TRANSPARENT
+        }
+
+        fun exitImmersive(activity: Activity) {
+            activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            activity.window.statusBarColor = Color.TRANSPARENT
+        }
+
+        fun hideStatusBar(activity: Activity) {
+            activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            activity.window.statusBarColor = Color.TRANSPARENT
+        }
+
         fun hideNavigationBar(activity: Activity) {
             hideNavigationBar(activity, Color.TRANSPARENT, true)
         }
