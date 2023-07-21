cbb5260497a88b80ff5db63497a3db6f348341eb
yeyunlin <yeyunlin@cvte.com>
2023-07-21 10:51:34 +0800

    [feature][1.0.20] 优化监听器的注册解注册逻辑

--- utils/src/main/java/com/seewo/student/libutils/extension/PowerSaveExt.kt
+++ utils/src/main/java/com/seewo/student/libutils/extension/PowerSaveExt.kt
@@ -2,26 +2,29 @@ package com.seewo.student.libutils.extension
 
 import android.content.Context
 import android.database.ContentObserver
-import android.util.Log
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import com.seewo.student.libutils.utils.PowerSaveModeHelper
 
-fun Context?.observePowerSaveModeChange(lifecycleOwner: LifecycleOwner, contentObserver: ContentObserver) {
-    if (this == null) {
-        Log.e("PowerSave", "context is null, observePowerSaveModeChange fail!")
+fun Context?.observePowerSaveModeChange(owner: LifecycleOwner, contentObserver: ContentObserver) {
+    if (this == null || owner.lifecycle.currentState == Lifecycle.State.DESTROYED) {
         return
     }
 
     contentResolver.registerContentObserver(PowerSaveModeHelper.settingsUriOfPowerSaveMode, true, contentObserver)
-    lifecycleOwner.lifecycle.addObserver(LifecycleInterObserver(this, contentObserver))
+    owner.lifecycle.addObserver(LifecycleInterObserver(this, owner, contentObserver))
 }
 
 
-internal class LifecycleInterObserver(val context: Context, private val contentObserver: ContentObserver) : LifecycleEventObserver {
+internal class LifecycleInterObserver(
+    val context: Context,
+    private val owner: LifecycleOwner,
+    private val contentObserver: ContentObserver
+) : LifecycleEventObserver {
     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
         if (event == Lifecycle.Event.ON_DESTROY) {
+            owner.lifecycle.removeObserver(this)
             context.contentResolver.unregisterContentObserver(contentObserver)
         }
     }
